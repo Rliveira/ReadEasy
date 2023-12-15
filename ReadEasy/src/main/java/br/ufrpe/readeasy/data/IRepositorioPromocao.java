80bbe2d772d204a5c51c3bde52c1527b81fbbf03
Louise <lvathayd@gmail.com>
2023-12-15 16:55:59 -0300

    Adicionando RepositorioPromocao e IRepositorioPromocao.

---
+++ ReadEasy/src/main/java/br/ufrpe/readeasy/data/IRepositorioPromocao.java
@@ -0,0 +1,28 @@
+package br.ufrpe.readeasy.data;
+
+import br.ufrpe.readeasy.beans.Promocao;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface IRepositorioPromocao {
+
+    public void inserir(Promocao promocao);
+
+    public void remover(Promocao promocao);
+
+    public void atualizar(Promocao promocao, String titulo, int porcentagemDeDesconto, int qtdMinimaDeLivros,
+                          LocalDate dataDeExpiracao, boolean ativa);
+
+    public List<Promocao> listarTodasPromocoes();
+
+    public List<Promocao> listarTodasPromocoesAtivas();
+
+    boolean existePromocao(String id);
+
+    Promocao buscarPromocao(String id);
+
+    String gerarId();
+
+}
+
---
+++ ReadEasy/src/main/java/br/ufrpe/readeasy/data/RepositorioPromocao.java
@@ -0,0 +1,99 @@
+package br.ufrpe.readeasy.data;
+
+
+import br.ufrpe.readeasy.beans.Promocao;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class RepositorioPromocao implements IRepositorioPromocao{
+
+    private static IRepositorioPromocao instance;
+    private ArrayList<Promocao> promocoes;
+
+    public RepositorioPromocao() {
+        this.promocoes = new ArrayList<>();
+    }
+
+    public static IRepositorioPromocao getInstance(){
+        if (instance == null) {
+            instance = new RepositorioPromocao();
+        }
+        return instance;
+    }
+    @Override
+    public void inserir(Promocao promocao){
+        promocoes.add(promocao);
+    }
+
+    @Override
+    public void remover(Promocao promocao){
+        promocoes.remove(promocao);
+    }
+
+    @Override
+    public void atualizar(Promocao promocao, String titulo, int porcentagemDeDesconto, int qtdMinimaDeLivros,
+                          LocalDate dataDeExpiracao, boolean ativa){
+        promocao.setTitulo(titulo);
+        promocao.setPorcentagemDeDesconto(porcentagemDeDesconto);
+        promocao.setQtdMinimaDeLivros(qtdMinimaDeLivros);
+        promocao.setDataDeExpiracao(dataDeExpiracao);
+        promocao.setAtiva(ativa);
+    }
+
+    @Override
+    public List<Promocao> listarTodasPromocoes(){
+        return promocoes;
+    }
+
+    @Override
+    public List<Promocao> listarTodasPromocoesAtivas(){
+
+        ArrayList<Promocao> listaDePromocoesAtivas = new ArrayList<>();
+        for (Promocao promocao : promocoes){
+            if (promocao.isAtiva()){
+                listaDePromocoesAtivas.add(promocao);
+            }
+        }
+        return listaDePromocoesAtivas;
+    }
+
+    @Override
+    public boolean existePromocao(String id){
+        boolean existe = false;
+        for(Promocao promocao : promocoes){
+            if(promocao.getId().equals(id)) {
+                existe = true;
+            }
+        }
+        return existe;
+    }
+
+    @Override
+    public Promocao buscarPromocao(String id){
+        Promocao promocao1 = null;
+        for(Promocao promocao : promocoes){
+            if(promocao.getId().equals(id)) {
+                promocao1 = promocao;
+            }
+        }
+        return promocao1;
+    }
+
+    @Override
+    public String gerarId() {
+        String caracteres = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        StringBuilder sb = new StringBuilder();
+
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            int index = random.nextInt(caracteres.length());
+            char caractere = caracteres.charAt(index);
+            sb.append(caractere);
+        }
+
+        return sb.toString();
+    }
+}
