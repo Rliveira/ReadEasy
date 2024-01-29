7fc29fa9660f94df8d59c46953e2b5d8afcc771e
João Victor M. B. da Silva <120438464+Jvfrostbr@users.noreply.github.com>
2024-01-29 16:29:32 -0300

    Adicionando LoginController

---
+++ ReadEasy/src/main/java/br/ufrpe/readeasy/gui/LoginController.java
@@ -0,0 +1,19 @@
+package br.ufrpe.readeasy.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    @FXML
+    private TextField tfUsuario;
+    @FXML
+    private TextField tfSenha;
+    @FXML
+    private Label lblCadastreSe;
+    @FXML
+    private Button btnLogin;
+
+}
