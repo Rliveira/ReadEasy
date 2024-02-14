794c9bdba5f853b2ac373b0ea22f47d53dd28b58
Louise <lvathayd@gmail.com>
2024-02-14 11:47:03 -0300

    Adaptando os controladores de Endereço às mudanças.

--- ReadEasy/src/main/java/br/ufrpe/readeasy/business/ControladorEndereco.java
+++ ReadEasy/src/main/java/br/ufrpe/readeasy/business/ControladorEndereco.java
@@ -4,6 +4,7 @@ import br.ufrpe.readeasy.beans.Cliente;
 import br.ufrpe.readeasy.beans.Endereco;
 import br.ufrpe.readeasy.data.*;
 import br.ufrpe.readeasy.exceptions.ClienteNuloException;
+import br.ufrpe.readeasy.exceptions.EnderecoInexistenteException;
 import br.ufrpe.readeasy.exceptions.EnderecoNuloException;
 import br.ufrpe.readeasy.exceptions.UsuarioInexistenteException;
 
@@ -46,6 +47,39 @@ public class ControladorEndereco implements IControladorEndereco{
             throw new ClienteNuloException(cpf);
         }
     }
+
+    public void atualizarEndereco(Endereco endereco, int cep, String rua, String bairro, String cidade, String estado)
+            throws EnderecoNuloException {
+        if (endereco != null) {
+
+            if (cep > 0 && endereco.getCep() != cep) {
+                endereco.setCep(cep);
+            }
+            if (rua != null && !rua.isEmpty() && !endereco.getRua().equals(rua)) {
+                endereco.setRua(rua);
+            }
+            if (bairro != null && !bairro.isEmpty() && !endereco.getBairro().equals(bairro)) {
+                endereco.setBairro(bairro);
+            }
+            if (cidade != null && !cidade.isEmpty() && !endereco.getCidade().equals(cidade)) {
+                endereco.setCidade(cidade);
+            }
+            if (estado != null && !estado.isEmpty() && !endereco.getEstado().equals(estado)) {
+                endereco.setEstado(estado);
+            }
+
+
+            repEnderecos.atualizarEndereco(endereco, cep, rua, bairro, cidade, estado);
+
+            repEnderecos.salvarArquivo();
+
+        } else {
+            throw new EnderecoNuloException(endereco);
+        }
+    }
+
+
+
 
     public void removerEnderecoCliente(String cpf, Endereco endereco) throws UsuarioInexistenteException, EnderecoNuloException,
             ClienteNuloException {
--- ReadEasy/src/main/java/br/ufrpe/readeasy/business/IControladorEndereco.java
+++ ReadEasy/src/main/java/br/ufrpe/readeasy/business/IControladorEndereco.java
@@ -1,7 +1,9 @@
+
 package br.ufrpe.readeasy.business;
 
 import br.ufrpe.readeasy.beans.Endereco;
 import br.ufrpe.readeasy.exceptions.ClienteNuloException;
+import br.ufrpe.readeasy.exceptions.EnderecoInexistenteException;
 import br.ufrpe.readeasy.exceptions.EnderecoNuloException;
 import br.ufrpe.readeasy.exceptions.UsuarioInexistenteException;
 
@@ -12,6 +14,8 @@ public interface IControladorEndereco {
     void adicionarEnderecoCliente(String cpf, Endereco endereco) throws UsuarioInexistenteException, EnderecoNuloException,
             ClienteNuloException;
 
+    public void atualizarEndereco (Endereco endereco, int cep, String rua, String bairro, String cidade, String estado) throws
+            EnderecoNuloException, EnderecoInexistenteException;
     void removerEnderecoCliente(String cpf, Endereco endereco) throws UsuarioInexistenteException, EnderecoNuloException,
             ClienteNuloException;
 
