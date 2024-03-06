3427fee08b6ceaea53727803744194eb269a9400
João Victor M. B. da Silva <120438464+Jvfrostbr@users.noreply.github.com>
2024-03-06 17:44:03 -0300

    Removendo exceção

--- ReadEasy/src/main/java/br/ufrpe/readeasy/exceptions/QuantidadeInvalidaException.java
+++
@@ -1,7 +0,0 @@
-package br.ufrpe.readeasy.exceptions;
-
-public class QuantidadeInvalidaException extends Exception {
-    public QuantidadeInvalidaException() {
-        super("A quantidade que você digitou é Inválida");
-    }
-}
