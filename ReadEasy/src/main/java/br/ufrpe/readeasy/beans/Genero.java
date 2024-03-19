1edefbdd5219d08872fe6e6a93ca5886579384b5
João Victor M. B. da Silva <120438464+Jvfrostbr@users.noreply.github.com>
2024-03-19 03:15:26 -0300

    adicionando e ajustando gêneros em Genero

--- ReadEasy/src/main/java/br/ufrpe/readeasy/beans/Genero.java
+++ ReadEasy/src/main/java/br/ufrpe/readeasy/beans/Genero.java
@@ -1,20 +1,26 @@
 package br.ufrpe.readeasy.beans;
 public enum Genero {
+    ACADEMICO("Acadêmico"),
     ACAO("Ação"),
+    AUTOBIOGRAFIA("Autobiografia"),
     AVENTURA("Aventura"),
-    SUSPENSE("Suspense"),
-    TERROR("Terror"),
+    CRITICA_SOCIAL("Crítica social"),
+    COMEDIA("Comédia"),
+    DRAMA("Drama"),
+    EPICO("Épico"),
     FANTASIA("Fantasia"),
+    FICCAO("Ficção"),
+    HISTORICO("Histórico"),
+    HISTORIA_EM_QUADRINHOS("História em quadrinhos"),
     INFANTO_JUVENIL("Infantojuvenil"),
-    FICCAO_CIENTIFICA("Ficção Científica"),
+    LITERATURA_CLASSICA("Literatura clássica"),
+    POESIA("Poesia"),
     NAO_FICCAO("Não Ficção"),
     ROMANCE("Romance"),
-    ACADEMICO("Acadêmico"),
-    COMEDIA("Comédia"),
-    CULINARIA("Culinária"),
-    AUTOBIOGRAFIA("Autobiografia"),
-    ECONOMIA("Economia"),
-    LITERATURA("Literatura");
+    RELIGIOSO("Religioso"),
+    SOBRENATURAL("Sobrenatural"),
+    SUSPENSE("Suspense"),
+    TERROR("Terror");
 
     private final String descricaoEnum;
 
