106c572f34199ab5c96bbf83dc1ce4a6a7554911
João Victor M. B. da Silva <120438464+Jvfrostbr@users.noreply.github.com>
2024-12-24 12:58:19 -0300

    atualizando o arquivo module-info.java

--- ReadEasy/src/main/java/module-info.java
+++ ReadEasy/src/main/java/module-info.java
@@ -5,7 +5,6 @@ module com.example.readeasy {
         requires org.controlsfx.controls;
             requires com.dlsc.formsfx;
                     requires org.kordamp.bootstrapfx.core;
-    requires fontawesomefx;
 
     opens br.ufrpe.readeasy to javafx.fxml;
     opens br.ufrpe.readeasy.gui to javafx.fxml;
